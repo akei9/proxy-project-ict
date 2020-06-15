3fa55b22f40cfe346fabab1df4bd27867fcd5cf7
Adrian Kucharczyk <adrian.kucharczyk@outlook.com>
2020-06-15 11:58:20 +0200

    Update DynamicChart.java

--- src/proxyproject/DynamicChart.java
+++ src/proxyproject/DynamicChart.java
@@ -20,7 +20,6 @@ import org.jfree.data.xy.XYDataset;
 import org.jfree.ui.ApplicationFrame;
 
 public class DynamicChart extends ApplicationFrame implements ActionListener {
-
     private TimeSeries series;
     private TimeSeries series1;
     private TimeSeries series2;
@@ -33,7 +32,6 @@ public class DynamicChart extends ApplicationFrame implements ActionListener {
     private Timer timer = new Timer(1000, this);
 
     public DynamicChart(final String title) {
-
         super(title);
         this.series = new TimeSeries("Data summary", Millisecond.class);
         this.series1 = new TimeSeries("Text data", Millisecond.class);
@@ -49,25 +47,29 @@ public class DynamicChart extends ApplicationFrame implements ActionListener {
         dataset.addSeries(series4);
         
         final JFreeChart chart = createChart(dataset);
-
         timer.setInitialDelay(1000);
         chart.setBackgroundPaint(Color.LIGHT_GRAY);
 
         final JPanel content = new JPanel(new BorderLayout());
         final ChartPanel chartPanel = new ChartPanel(chart);
-
         content.add(chartPanel);
         chartPanel.setPreferredSize(new java.awt.Dimension(800, 500));
         setContentPane(content);
         timer.start();
-
     }
 
     private JFreeChart createChart(final XYDataset dataset) {
+        final JFreeChart result = ChartFactory.createTimeSeriesChart(
+            "Web Statistics", 
+            "Time [s]", 
+            "Data [bytes]", 
+            dataset, 
+            true, 
+            true, 
+            false
+        );
         
-        final JFreeChart result = ChartFactory.createTimeSeriesChart("Web Statistics", "Time [s]", "Data [bytes]", dataset, true, true, false);
         final XYPlot plot = result.getXYPlot();
-        
         plot.setBackgroundPaint(new Color(0xffffe0));
         plot.setDomainGridlinesVisible(true);
         plot.setDomainGridlinePaint(Color.lightGray);
@@ -76,23 +78,19 @@ public class DynamicChart extends ApplicationFrame implements ActionListener {
 
         ValueAxis xaxis = plot.getDomainAxis();
         xaxis.setAutoRange(true);
-        
         xaxis.setFixedAutoRange(60000.0);  // 60 seconds
         xaxis.setVerticalTickLabels(true);
 
         ValueAxis yaxis = plot.getRangeAxis();
 //        yaxis.setRange(0.0, 300.0);
         yaxis.setAutoRange(true);
-        
+
         return result;
-        
     }
 
     @Override
     public void actionPerformed(final ActionEvent e) {
-
         int sumValue = Threads.transferSum;
-//        int sumTransferValue = Threads.dataTransfer;
         int textTransferValue = Threads.textTransfer;
         int appTransferValue = Threads.appTransfer;
         int imageTranferValue = Threads.imageTransfer;
@@ -107,6 +105,4 @@ public class DynamicChart extends ApplicationFrame implements ActionListener {
 //        this.series.addOrUpdate(new Millisecond(), value);
 //        System.out.println("Current Time : " + now.toString() + ", Current Value : " + sumTransferValue);
     }
-
 }  
-
